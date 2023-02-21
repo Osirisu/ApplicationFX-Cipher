a69ad1f1eaaea1c47468a1ca87a2d37b45ba284f
Mirael <miraeleso@gmail.com>
2023-02-21 11:52:29 +0600

    Savepoint

--- src/main/java/com/example/appcipher/HelloApplication.java
+++ src/main/java/com/example/appcipher/HelloApplication.java
@@ -22,8 +22,7 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    public void start(Stage stage) {
         GridPane gridPane = createRegistrationFormPane();
         createUIControls(gridPane);
 
