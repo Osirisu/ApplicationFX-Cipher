dc6c5220fc4ea211d676fe6a5db318b8800f1f5d
Mirael <miraeleso@gmail.com>
2023-03-15 20:20:56 +0600

    File editor

--- src/main/java/com/example/appcipher/HelloApplication.java
+++ src/main/java/com/example/appcipher/HelloApplication.java
@@ -16,11 +16,21 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 public class HelloApplication extends Application {
+    private Stage javaFXC;
+    private String inputFilePath;
+    private String outputFilePath;
     @Override
     public void start(Stage stage) {
+        inputFilePath = "/home/mirael/IdeaProjects/appCipher/src/main/java/com/example/appcipher/input.txt";
+        outputFilePath = "/home/mirael/IdeaProjects/appCipher/src/main/java/com/example/appcipher/output.txt";
+
         GridPane gridPane = createRegistrationFormPane();
         createUIControls(gridPane);
 
@@ -30,6 +40,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Cipher");
         stage.setScene(scene);
         stage.show();
+        javaFXC = stage;
     }
     public static void main(String[] args) {
         launch();
@@ -89,23 +100,57 @@ public class HelloApplication extends Application {
         RadioButton buttonDecryption = new RadioButton("Дешифрование");
         gridPane.add(buttonDecryption, 1, 3);
         buttonDecryption.setToggleGroup(groupCrypt);
+        //File
+        CheckBox checkOutputFile = new CheckBox("Из файла");
+        gridPane.add(checkOutputFile, 0,4);
+
+        Button openOutputFile = new Button("Выбрать файл");
+        gridPane.add(openOutputFile, 1,4);
+
+        CheckBox checkInputFile = new CheckBox("В файл");
+        gridPane.add(checkInputFile, 2,4);
+
+        Button openInputFile = new Button("В файл");
+        gridPane.add(openInputFile, 3, 4);
         //
         Label resultText = new Label("Результат");
-        gridPane.add(resultText, 0, 4);
+        gridPane.add(resultText, 0, 5);
         GridPane.setHalignment(resultText, HPos.LEFT);
 
         TextField textResult = new TextField();
-        gridPane.add(textResult, 1, 4);
+        gridPane.add(textResult, 1, 5);
         textResult.setEditable(false);
         //
         Button crypt = new Button("Начать операцию");
         crypt.setDefaultButton(false);
-        gridPane.add(crypt, 0, 5, 2, 1);
+        gridPane.add(crypt, 0, 6, 2, 1);
         GridPane.setHalignment(crypt, HPos.CENTER);
+        //Buttons
+        openOutputFile.setOnAction(actionEvent -> {
+            if (checkOutputFile.isSelected()) {
+                String path = getPath();
 
+                textField.setText(path);
+                inputFilePath = path;
+            }
+        });
+        openInputFile.setOnAction(actionEvent -> {
+            if (checkInputFile.isSelected()){
+                outputFilePath = getPath();
+            }
+        });
         crypt.setOnAction(actionEvent -> {
             String txt = textField.getText();
             String result = "";
+
+            if (checkOutputFile.isSelected()){
+                try {
+                    txt = outputText(inputFilePath);
+                }catch (Exception e){
+                    e.printStackTrace();
+                    return;
+                }
+            }
 
             if (txt.equals("")) {
                 textResult.setText("");
@@ -131,6 +176,58 @@ public class HelloApplication extends Application {
                 result = cipher.decryption();
 
             textResult.setText(result);
+
+            if (checkInputFile.isSelected()){
+                inputText(outputFilePath, result);
+            }
         });
+    }
+
+    private String getPath(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Выбрать текст");
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Текст", "*.txt");
+        fileChooser.getExtensionFilters().add(filter);
+        File file = fileChooser.showOpenDialog(javaFXC);
+
+        return file.getPath();
+    }
+
+    private void inputText(String path, String text){
+        if (path.isEmpty())
+            path = outputFilePath;
+
+        File file = new File(path);
+        try (PrintWriter out = new PrintWriter(file, StandardCharsets.UTF_8))
+        {
+            out.print(text);
+            System.out.println("Successfully written data to the file");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private String outputText(String path){
+        if (path.isEmpty())
+            path = inputFilePath;
+
+        try {
+            File file = new File(path);
+            //создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            String text = "";
+            while (line != null) {
+                text += line + "\n";
+                // считываем остальные строки в цикле
+                line = reader.readLine();
+            }
+            return text.replace("\n"," ");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }
