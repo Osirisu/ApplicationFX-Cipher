ebc0ff01ba778e3e6ae059df949a52263d23d164
Mirael <miraeleso@gmail.com>
2023-04-12 12:20:19 +0600

    Created new file

--- src/main/java/com/example/appcipher/HelloApplication.java
+++ src/main/java/com/example/appcipher/HelloApplication.java
@@ -23,10 +23,44 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.interfaces.ECPublicKey;
+import org.bouncycastle.jce.spec.ECParameterSpec;
+
+import java.io.UnsupportedEncodingException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
 public class HelloApplication extends Application {
     private Stage javaFXC;
     private String inputFilePath;
     private String outputFilePath;
+
+    private Label nameCipher;
+    private Label text;
+    private TextField textField;
+    private Label publicKey;
+    private TextField publicKeyText;
+    private Label signature;
+    private TextField signatureText;
+    private ToggleGroup groupLanguage;
+    private RadioButton englishLanguage;
+    private RadioButton russianLanguage;
+    private ToggleGroup groupCrypt;
+    private RadioButton buttonEncryption;
+    private RadioButton buttonDecryption;
+    private CheckBox checkOutputFile;
+    private Button openOutputFile;
+    private CheckBox checkInputFile;
+    private Button openInputFile;
+    private CheckBox integrityCheck;
+    private Label resultIntegrity;
+    private TextField resultIntegrityText;
+    private Label resultCrypto;
+    private TextField resultCryptoText;
+    private Button btnResult;
 
     public static void main(String[] args) {
         launch();
@@ -52,7 +86,7 @@ public class HelloApplication extends Application {
         GridPane gridPane = new GridPane();
 
         gridPane.setAlignment(Pos.CENTER);
-        gridPane.setPadding(new Insets(40, 40, 40, 40));
+        gridPane.setPadding(new Insets(50, 60, 50, 60));
 
         gridPane.setHgap(10);
         gridPane.setVgap(10);
@@ -68,69 +102,95 @@ public class HelloApplication extends Application {
         return gridPane;
     }
     private void createUIControls(GridPane gridPane){
+        int numberLines = 0;
         //Шапка
-        Label nameCipher = new Label("Шифр Тритемиуса");
+        nameCipher = new Label("Шифр Тритемиуса");
         nameCipher.setFont(Font.font("Arial", FontWeight.BOLD, 24));
 
-        gridPane.add(nameCipher, 0,0, 2, 1);
+        gridPane.add(nameCipher, 0,numberLines++, 2, 1);
         GridPane.setHalignment(nameCipher, HPos.CENTER);
         //
-        Label text = new Label("Введите текст: ");
-        gridPane.add(text, 0, 1);
+        text = new Label("Введите текст: ");
+        gridPane.add(text, 0, numberLines);
         GridPane.setHalignment(text, HPos.LEFT);
 
-        TextField textField = new TextField();
-        gridPane.add(textField, 1, 1);
-        //Линия 2
-        ToggleGroup groupLanguage = new ToggleGroup();
+        textField = new TextField();
+        gridPane.add(textField, 1, numberLines++);
+        //
+        publicKey = new Label("Public Key");
+        gridPane.add(publicKey, 0, numberLines);
 
-        RadioButton englishLanguage = new RadioButton("Английский");
-        gridPane.add(englishLanguage, 0,2);
+        publicKeyText = new TextField();
+        gridPane.add(publicKeyText, 1, numberLines++);
+        //
+        signature = new Label("SiqNature");
+        gridPane.add(signature, 0, numberLines);
+
+        signatureText = new TextField();
+        gridPane.add(signatureText, 1, numberLines++);
+        //Линия 2
+        groupLanguage = new ToggleGroup();
+
+        englishLanguage = new RadioButton("Английский");
+        gridPane.add(englishLanguage, 0,numberLines);
         englishLanguage.setToggleGroup(groupLanguage);
         englishLanguage.setSelected(true);
 
-        RadioButton russianLanguage = new RadioButton("Русский");
-        gridPane.add(russianLanguage, 1, 2);
+        russianLanguage = new RadioButton("Русский");
+        gridPane.add(russianLanguage, 1, numberLines++);
         russianLanguage.setToggleGroup(groupLanguage);
         //Линия 3
-        ToggleGroup groupCrypt = new ToggleGroup();
+        groupCrypt = new ToggleGroup();
 
-        RadioButton buttonEncryption = new RadioButton("Шифрование");
-        gridPane.add(buttonEncryption, 0, 3);
+        buttonEncryption = new RadioButton("Шифрование");
+        gridPane.add(buttonEncryption, 0, numberLines);
         buttonEncryption.setToggleGroup(groupCrypt);
         buttonEncryption.setSelected(true);
 
-        RadioButton buttonDecryption = new RadioButton("Дешифрование");
-        gridPane.add(buttonDecryption, 1, 3);
+        buttonDecryption = new RadioButton("Дешифрование");
+        gridPane.add(buttonDecryption, 1, numberLines++);
         buttonDecryption.setToggleGroup(groupCrypt);
         //File
-        CheckBox checkOutputFile = new CheckBox("Из файла");
-        gridPane.add(checkOutputFile, 0,4);
+        checkOutputFile = new CheckBox("Из файла");
+        gridPane.add(checkOutputFile, 0,numberLines);
 
-        Button openOutputFile = new Button("Выбрать файл");
+        openOutputFile = new Button("Выбрать файл");
         openOutputFile.setTooltip(new Tooltip("Выберите файл из которого\nнадо достать текст"));
-        gridPane.add(openOutputFile, 1,4);
+        gridPane.add(openOutputFile, 1,numberLines);
 
-        CheckBox checkInputFile = new CheckBox("В файл");
-        gridPane.add(checkInputFile, 2,4);
+        checkInputFile = new CheckBox("В файл");
+        gridPane.add(checkInputFile, 2,numberLines);
 
-        Button openInputFile = new Button("В файл");
+        openInputFile = new Button("В файл");
         openInputFile.setTooltip(new Tooltip("Выберите файл в который\nнадо записать результат операции"));
-        gridPane.add(openInputFile, 3, 4);
+        gridPane.add(openInputFile, 3, numberLines++);
         //
-        Label resultText = new Label("Результат");
-        gridPane.add(resultText, 0, 5);
-        GridPane.setHalignment(resultText, HPos.LEFT);
+        integrityCheck = new CheckBox("Проверка целостности");
+        integrityCheck.setTooltip(new Tooltip("Проверять ли на целостность?"));
+        gridPane.add(integrityCheck, 0, numberLines++, 2, 1);
+        GridPane.setHalignment(integrityCheck, HPos.CENTER);
+        //
+        resultIntegrity = new Label("Результат целостности: ");
+        gridPane.add(resultIntegrity, 0, numberLines);
 
-        TextField textResult = new TextField();
-        gridPane.add(textResult, 1, 5);
-        textResult.setEditable(false);
+        resultIntegrityText = new TextField();
+        gridPane.add(resultIntegrityText, 1, numberLines++);
         //
-        Button crypt = new Button("Начать операцию");
-        crypt.setDefaultButton(false);
-        gridPane.add(crypt, 0, 6, 2, 1);
-        GridPane.setHalignment(crypt, HPos.CENTER);
-        //Buttons
+        resultCrypto = new Label("Результат");
+        gridPane.add(resultCrypto, 0, numberLines);
+        GridPane.setHalignment(resultCrypto, HPos.LEFT);
+
+        resultCryptoText = new TextField();
+        gridPane.add(resultCryptoText, 1, numberLines++);
+        resultCryptoText.setEditable(false);
+        //
+        btnResult = new Button("Начать операцию");
+        btnResult.setDefaultButton(false);
+        gridPane.add(btnResult, 0, numberLines++, 2, 1);
+        GridPane.setHalignment(btnResult, HPos.CENTER);
+    }
+
+    private void controller(){
         openOutputFile.setOnAction(actionEvent -> {
             if (checkOutputFile.isSelected()) {
                 File myChoosenFile = getFile();
@@ -150,7 +210,7 @@ public class HelloApplication extends Application {
                     outputFilePath = myChoosenFile.getPath();
             }
         });
-        crypt.setOnAction(actionEvent -> {
+        btnResult.setOnAction(actionEvent -> {
             String txt = textField.getText();
             String result = "";
 
@@ -163,7 +223,7 @@ public class HelloApplication extends Application {
                 }
             }
             if (txt.equals("")) {
-                textResult.setText("");
+                resultCryptoText.setText("");
                 return;
             }
 
@@ -187,7 +247,7 @@ public class HelloApplication extends Application {
             if (operation.equals(buttonDecryption))
                 result = cipher.decryption();
 
-            textResult.setText(result);
+            resultCryptoText.setText(result);
 
             if (checkInputFile.isSelected()){
                 try {
@@ -200,7 +260,7 @@ public class HelloApplication extends Application {
         });
     }
 
-    protected File getFile(){
+    private File getFile(){
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Выбрать текст");
 
@@ -214,7 +274,6 @@ public class HelloApplication extends Application {
 
         return file;
     }
-
     private String createCopyFile(File myFile){
         String oldNameFile = myFile.getName();
         String newNameFile = myFile.getName().replace(".txt","");
@@ -278,5 +337,62 @@ public class HelloApplication extends Application {
             e.printStackTrace();
             return "";
         }
+    }
+
+    // ----------------------------------------------------------------
+    // Bounty Castle methods
+
+    public static byte[] GenerateSignature(String plaintext, PrivateKey keys)
+            throws SignatureException, UnsupportedEncodingException,
+            InvalidKeyException, NoSuchAlgorithmException,
+            NoSuchProviderException
+    {
+        Signature ecdsaSign = Signature.getInstance("SHA256withECDSA", "BC");
+        ecdsaSign.initSign(keys);
+        ecdsaSign.update(plaintext.getBytes("UTF-8"));
+        return ecdsaSign.sign();
+    }
+
+    public static boolean ValidateSignature(String plaintext, PublicKey pair, byte[] signature) throws SignatureException,
+            InvalidKeyException, UnsupportedEncodingException,
+            NoSuchAlgorithmException, NoSuchProviderException
+    {
+        Signature ecdsaVerify = Signature.getInstance("SHA256withECDSA", "BC");
+        ecdsaVerify.initVerify(pair);
+        ecdsaVerify.update(plaintext.getBytes("UTF-8"));
+        return ecdsaVerify.verify(signature);
+    }
+
+    public static KeyPair GenerateKeys() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException
+    {
+        //  Other named curves can be found in http://www.bouncycastle.org/wiki/display/JA1/Supported+Curves+%28ECDSA+and+ECGOST%29
+        ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec("B-571");
+        KeyPairGenerator g = KeyPairGenerator.getInstance("ECDSA", "BC");
+        g.initialize(ecSpec, new SecureRandom());
+        return g.generateKeyPair();
+    }
+    // My methods
+
+    private static String getStringFromPublicKey(KeyPair key){
+        return Base64.getEncoder().encodeToString(key.getPublic().getEncoded());
+    }
+    private static String getStringFromPublicKey(PublicKey key){
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    private static PublicKey getPublicKeyFromSting(String stringPublicKey)
+            throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException
+    {
+        byte[] bytesPublicKey = Base64.getDecoder().decode(stringPublicKey);
+
+        KeyFactory factory = KeyFactory.getInstance("ECDSA", "BC");
+        return (ECPublicKey) factory.generatePublic(new X509EncodedKeySpec(bytesPublicKey));
+    }
+
+    private static String bytearrayToString(byte[] bytes){
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+    public static byte[] stringToByteArray(String string){
+        return Base64.getDecoder().decode(string);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.example.appcipher {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires org.bouncycastle.provider;
+    requires java.sql;
 
     opens com.example.appcipher to javafx.fxml;
     exports com.example.appcipher;
